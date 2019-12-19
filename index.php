20e126df419aeec939e51770a4f007639b88d74a
Tyffany <robbes.tyffany@gmail.com>
2019-12-19 10:12:30 +0100

    Création du rooter

--- index.php
+++ index.php
@@ -1,12 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog de Jean Forteroche</title>
-</head>
-<body>
+<?php 
+require('vendor/autoload.php');
+require('controller/frontend.php');
+
+use Model\PostManager;
+
+try {
+    if (isset($_GET['action'])) {
+        if($_GET['action'] == 'listPosts') {
+            listPosts();
+        }
+        elseif($_GET['action'] == 'post') {
+            if(isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
+            }
+            else {
+                throw new Exeption('Erreur : aucun identifiant de billet envoyé');
+            }
+        }
+        elseif($_GET['action'] == 'addComment') {
+            if(isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                } else {
+                    throw new Exeption('Erreur : tous les champs ne sont pas remplis');
+                }
+            } else {
+                throw new Exception('Erreur : aucun identifiant de billet envoyé');
+            }
     
-</body>
-</html> 
+        }
+        elseif($_GET['action'] == 'commentView') {
+            if(isset($_GET['commentId']) && $_GET['commentId'] > 0) {
+                commentView($_GET['commentId']);
+            } else {
+                throw new Exception('Erreur : aucun identifiant de commentaire envoyé');
+            }
+        } elseif ($_GET['action'] == 'modifyComment') {
+            if(isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['comment'])) {
+                    modifyComment($_GET['id'], $_POST['comment']);
+                } else {
+                    throw new Exeption('Erreur : le commentaire n\'a pu être modifié');
+                }
+                
+            }
+        }
+    } else {
+        listPosts();
+    }
+} catch (Exception $e) {
+    echo 'Erreur : ' . $e->getMessage();
+}
+
+
+
+
+
