857241d90d5878a3fb5eaf37166abb7d93c0fe3a
Tyffany <robbes.tyffany@gmail.com>
2019-12-19 10:16:27 +0100

    Ajout du model contenant trois classes principales : CommentManager / Manager / PostManager

---
+++ model/PostManager.php
@@ -0,0 +1,21 @@
+<?php
+Namespace Model;
+
+class PostManager extends Manager {
+    public function getPosts() 
+    {
+        $db = $this->dbConnect();
+        $posts = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date,\'%d/%m/%Y à %Hh%imin%ss\')AS date_creation_fr FROM posts ORDER BY creation_date DESC');
+        return $posts;
+    }
+
+    public function getPost($postId)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
+
+        return $post;
+    }
+}
